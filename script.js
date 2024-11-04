dfa2f9cbe9a524b30c6971318b08089e0b39f79e
jdvincent88 <jdvincent88@gmail.com>
2024-11-04 15:28:54 -0500

    Added files via upload
    First files added

---
+++ script.js
@@ -0,0 +1,10 @@
+// Simple cart addition example
+let cart = [];
+
+function addToCart(product) {
+    cart.push(product);
+    alert(product.name + " added to cart!");
+}
+
+// Display cart items in the console for testing
+console.log(cart);
